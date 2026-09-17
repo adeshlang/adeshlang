60feeb23964c727a85285a33b40410818e8dc09f
ajaytainwala-dev <ajaytainwala@gmail.com>
2026-09-17 13:17:48 +0530

    Add support for armeabi-v7a architecture in mobile release workflow; update build.gradle.kts

--- mobile/flutter/android/app/build.gradle.kts
+++ mobile/flutter/android/app/build.gradle.kts
@@ -25,10 +25,6 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-
-        ndk {
-            abiFilters.addAll(setOf("arm64-v8a", "x86_64"))
-        }
     }
 
     sourceSets {
